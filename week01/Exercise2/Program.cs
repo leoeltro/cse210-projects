5234e8126f60475f7f3e708cbf174f4856a41233
Leonardo Barbosa Lima Gouvea <leoeltro@yahoo.com.br>
2026-01-12 03:59:56 -0300

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 2 Project.");
 
          Console.Write("Enter the student's grade percentage: ");
          string grade = Console.ReadLine();
