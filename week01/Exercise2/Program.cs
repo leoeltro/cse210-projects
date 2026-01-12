324c941c3fd3da28c88a0e153d02eb6cade27561
Leonardo Barbosa Lima Gouvea <leoeltro@yahoo.com.br>
2026-01-12 03:02:06 -0300

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+         Console.Write("Enter the student's grade percentage: ");
+         string grade = Console.ReadLine();
+         int grade_number= int.Parse(grade);
+
+         string grade_letter = "";
+
+         if(grade_number >= 90)
+                grade_letter= "A";
+
+         else if(grade_number >= 80)
+                grade_letter= "B";
+
+         else if(grade_number >= 70)
+                grade_letter= "C";                
+
+         else if(grade_number >= 60)
+                grade_letter= "D";
+
+         else if(grade_number <= 60)
+                grade_letter= "f";  
+
+        else
+                Console.WriteLine("The number entered is outside the allowed range!");
+
+
+        string grade_symbol= "";
+        int rest = grade_number % 10;
+
+        if (rest < 3 && grade_number >= 60)
+            grade_symbol= "-";
+
+        if (rest >= 7 && grade_number >= 60 && grade_number < 90)
+            grade_symbol= "+";           
+
+
+        Console.Write($"The letter grade used for grading was: {grade_letter}{grade_symbol}");
+
     }
 }
